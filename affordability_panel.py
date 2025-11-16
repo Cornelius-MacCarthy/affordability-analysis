80063aa1c0592ddad15405722bf196383067a2dc
Cornelius-MacCarthy <cornelius.maccarthy@ucdconnect.ie>
2025-11-16 10:15:32 +0000

    loading the CSVs

--- affordability_panel.py
+++ affordability_panel.py
@@ -15,3 +15,8 @@ F_INCOME_1 = os.path.join(DL, "Median_income_2004-2019.csv")
 F_INCOME_2 = os.path.join(DL, "Median_income_2020-2024.csv")
 F_RENT     = os.path.join(DL, "Rents_per_month_by_location.csv")
 F_PPI      = os.path.join(DL, "Property_Price_Index.csv")
+# Loading in CSV files basaed on file paths
+income1 = pd.read_csv(F_INCOME_1)
+income2 = pd.read_csv(F_INCOME_2)
+rents   = pd.read_csv(F_RENT)
+ppi     = pd.read_csv(F_PPI)
